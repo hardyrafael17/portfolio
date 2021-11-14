2cfc0262254fe150e3f8f845cf3b705083d50333
hardyrafael17 <hardy.rafael17@gmail.com>
2021-11-14 13:07:45 +0100

    update

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { FaEnvelopeSquare, FaGithub, FaLinkedin, FaPhone } from "react-icons/fa";
 
-export const  Contact = () => {
-      return (
+export const Contact = () => {
+  return (
+    <div>
+      <section id="contact">
+        <h2>Contact</h2>
         <div>
-            <section id="contact">
-              <h2>Contact</h2>
-              <div>
-                <a href="mailto:hardy.rafael17@gmail.com" ><FaEnvelopeSquare className="fab icon"/></a>
-                <a href="https://www.linkedin.com/in/jimenezhardy/" rel="noreferrer" target="_blank"><FaLinkedin className="fab icon"/></a>
-                <a href="https://github.com/hardyrafael17" rel="noreferrer" target="_blank"><FaGithub className="fab icon" /></a>
-                <a href="tel:+34612634"><FaPhone className="fab icon"/></a>
-              </div>
-            </section>
+          <a href="mailto:hardy.rafael17@gmail.com" ><FaEnvelopeSquare className="fab icon" /></a>
+          <a href="https://www.linkedin.com/in/jimenezhardy/" rel="noreferrer" target="_blank"><FaLinkedin className="fab icon" /></a>
+          <a href="https://github.com/hardyrafael17" rel="noreferrer" target="_blank"><FaGithub className="fab icon" /></a>
+          <a href="tel:+34 641 61 26 34"><FaPhone className="fab icon" /></a>
         </div>
-      )
-    };
+      </section>
+    </div>
+  )
+};
